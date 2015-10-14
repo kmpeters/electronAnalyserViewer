9711b64965b3ec6409350ccab3de9aee819ee4bb
kpetersn <kmpeters@anl.gov>
2015-10-14 15:29:56 -0500

    Finally got the support working.

--- iocBoot/iocelectronAnalyserViewer/st.cmd
+++ iocBoot/iocelectronAnalyserViewer/st.cmd
@@ -4,6 +4,9 @@
 epicsEnvSet("PREFIX", "EAV1:")
 epicsEnvSet("PORT",   "EA1")
 epicsEnvSet("QSIZE",  "20")
+epicsEnvSet("XSIZE",  "1392")
+epicsEnvSet("YSIZE",  "1040")
+epicsEnvSet("NCHANS", "2048")
 
 #
 epicsEnvSet EPICS_CA_MAX_ARRAY_BYTES 100000000
@@ -14,17 +17,29 @@ errlogInit(20000)
 dbLoadDatabase("$(TOP)/dbd/electronAnalyserViewer.dbd")
 electronAnalyserViewer_registerRecordDeviceDriver(pdbbase)
 
-#
-drvAsynIPPortConfigure("$(PORT)","192.168.0.1:55555", 0, 0, 0)
-
 # Configure electron analyser viewer 
+# IP address specified in a PV: $(PREFIX):cam1:CONNECTION
+#   "tcp://192.168.0.1:55555"
+#   "tcp://164.54.118.4:8888"
+
 electronAnalyserViewerConfig("$(PORT)", 30, 25000000, 0, 0)
 
 #asynSetTraceMask("$(PORT)", 0, 0x1)
 #asynSetTraceIOMask("$(PORT)", 0, 0x0)
 
+# Load the ADBase PVs
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/ADBase.template",     "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+
+# Create a standard arrays plugin
+NDStdArraysConfigure("Image1", 3, 0, "$(PORT)", 0)
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=0")
+
+# This creates a waveform large enough for 1392x1040x1 (e.g. grayscale) arrays.
+# This waveform only allows transporting 64-bit images
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,TYPE=Float64,FTVL=DOUBLE,NELEMENTS=1447680")
+
 # Load the detector-specific PVs
-dbLoadRecords("$(TOP)/electronAnalyserViewerApp/Db/electronAnalyser.db","P=$(PREFIX),R=cam1:,PORT=$(PORT)")
+dbLoadRecords("$(TOP)/electronAnalyserViewerApp/Db/electronAnalyserViewer.db","P=$(PREFIX),R=cam1:,PORT=$(PORT)")
 
 # Load all the areaDetector plugins
 < "$(AREA_DETECTOR)/iocBoot/commonPlugins.cmd"
